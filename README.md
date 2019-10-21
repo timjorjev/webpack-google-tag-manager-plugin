9892b75f50016388f4fa8781d94a546290d23dc0
Tim Jorjev <dev@timjorjev.com>
2019-10-21 03:13:59 +0300

    Deploy a basic configuration

---
+++ README.md
@@ -0,0 +1,49 @@
+<div align="center">
+  <a href="https://github.com/webpack/webpack">
+    <img width="200" height="200"
+      src="https://webpack.js.org/assets/icon-square-big.svg">
+  </a>
+  <h1>Webpack Google Tag Manager Plugin</h1>
+  <p>A clean and easy way to add Google Tag Manager support to your web project.</p>
+</div>
+
+## Installation
+
+```bash
+  npm i --save-dev webpack-google-tag-manager-plugin
+```
+
+```bash
+  yarn add --dev webpack-google-tag-manager-plugin
+```
+
+## Examples
+Webpack configuration for setting up the plugin
+```Javascript
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import GoogleTagManagerPlugin from 'webpack-google-tag-manager-plugin'
+
+module.exports = {
+    ...
+    plugins: [
+        new HtmlWebpackPlugin({
+          ...
+        }),
+        new GoogleTagManagerPlugin({
+            id: 'your-key',
+            auth: 'your-auth-value',
+            preview: 'your-environment-value'
+        })
+    ],
+}
+
+```
+
+## [License](https://github.com/timjorjev/webpack-google-tag-manager-plugin/blob/master/LICENSE)
+
+Webpack Google Tag Manager Plugin is published under [MIT License](https://github.com/timjorjev/webpack-google-tag-manager-plugin/blob/master/LICENSE).
+
+## Collaboration
+
+If you have any questions, please contact me via [e-mail](mailto:dev@timjorjev.com). For [issues](https://github.com/timjorjev/webpack-google-tag-manager-plugin/issues), please [open an issue](https://github.com/timjorjev/webpack-google-tag-manager-plugin/issues/new)!
---
+++ index.js
@@ -0,0 +1,80 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { stripIndent } = require('common-tags')
+
+class GoogleTagManagerPlugin {
+
+  constructor(options) {
+
+    const userOptions = options || {};
+  
+    const defaultOptions = {
+      id: '',
+      events: '',
+      dataLayer: '',
+      dataLayerName: '',
+      auth: '',
+      preview: ''
+    }
+  
+    this.options = Object.assign(defaultOptions, userOptions);
+  
+    if (!userOptions.id) {
+      console.error(`The plugin option "id" has not been set.`)
+    }
+
+    this.snippets = {
+
+      script: stripIndent`
+          <!-- Google Tag Manager -->
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js',${JSON.stringify(this.options.events).slice(1, -1)}});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=${this.options.auth}&gtm_preview=${this.options.preview}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+          })(window,document,'script','${this.options.dataLayerName}','${this.options.id}');</script>
+          <!-- End Google Tag Manager -->
+      `,
+    
+      noScript: stripIndent`
+          <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${this.options.id}&gtm_auth=${this.options.auth}&gtm_preview=${this.options.preview}&gtm_cookies_win=x"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <!-- End Google Tag Manager (noscript) -->
+      `,
+    
+      dataLayer: (() => {
+          let result = `window.${this.options.dataLayerName}=window.${this.options.dataLayerName}||[];`
+        
+          if (typeof this.options.dataLayer === `object` || this.options.dataLayer instanceof Object) {
+            result += `window.${this.options.dataLayerName}.push(${JSON.stringify(
+              this.options.dataLayer
+            )});`
+          } else {
+            console.error(
+              `The plugin option "dataLayer" should be a plain object. "${this.options.dataLayer}" is not valid.`
+            )
+          }
+          return stripIndent`${result}`
+        })()
+    }
+  }
+
+  apply(compiler) {
+    compiler.hooks.compilation.tap('GoogleTagManagerPlugin', (compilation) => {
+      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+        'GoogleTagManagerPlugin', (htmlPluginData, callback) => {
+          const headRegExp = /(<\/head>)/i
+          const bodyRegExp = /(<body\s*>)/i
+
+          if (this.options.dataLayer)
+            htmlPluginData.html = htmlPluginData.html.replace(headRegExp, match => this.snippets.dataLayer + match)
+          htmlPluginData.html = htmlPluginData.html.replace(headRegExp, match => this.snippets.script + match)
+          htmlPluginData.html = htmlPluginData.html.replace(bodyRegExp, match => match + this.snippets.noScript)
+          callback(null, htmlPluginData)
+        }
+      )
+    })
+  }
+
+}
+
+module.exports = GoogleTagManagerPlugin
